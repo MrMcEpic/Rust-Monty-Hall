653da47042b60e135e2d53db8349fb0e1ab2d83f
MrMcEpic <45011453+MrMcEpic@users.noreply.github.com>
2019-11-22 15:51:06 -0500

    Simplified code

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,19 @@
-#![feature(vec_remove_item)]
-#![feature(drain_filter)]
+#![feature(vec_remove_item, drain_filter)]
 use rand::Rng;
 use std::io;
 
-pub struct Boxes {
-	box_1: bool,
-	box_2: bool,
-	box_3: bool,
+fn new_boxes() -> [bool; 3] {
+	let mut boxes = [false, false, false];
+	boxes[rand::thread_rng().gen_range(0, 3)] = true;
+	boxes
 }
 
-impl Boxes {
-	fn new() -> Boxes {
-		let boxes = Boxes {
-			box_1: false,
-			box_2: false,
-			box_3: false,
-		};
-		match rand::thread_rng().gen_range(0, 3) {
-			0 => Boxes {
-				box_1: true,
-				..boxes
-			},
-			1 => Boxes {
-				box_2: true,
-				..boxes
-			},
-			2 => Boxes {
-				box_3: true,
-				..boxes
-			},
-			_ => panic!("Error gening box"),
-		}
-	}
-}
-
-fn no_switch_box(tries: u32) -> (u64, u64) {
+fn no_switch_box(tries: u64) -> (u64, u64) {
 	let mut val = 0;
 	let mut val_lose = 0;
 	for _ in 0..tries {
-		let boxes = Boxes::new();
-		let pick = {
-			match rand::thread_rng().gen_range(0, 3) {
-				0 => boxes.box_1,
-				1 => boxes.box_2,
-				2 => boxes.box_3,
-				_ => panic!("Error picking box on no switch"),
-			}
-		};
+		let boxes = new_boxes();
+		let pick = boxes[rand::thread_rng().gen_range(0, 3)];
 		if pick {
 			val += 1;
 		} else {
@@ -56,34 +23,25 @@ fn no_switch_box(tries: u32) -> (u64, u64) {
 	(val, val_lose)
 }
 
-fn switch_box(tries: u32) -> (u64, u64) {
+fn switch_box(tries: u64) -> (u64, u64) {
 	let mut val = 0;
 	let mut val_lose = 0;
 	for _ in 0..tries {
-		let boxes = Boxes::new();
+		let boxes = new_boxes();
 		let first_pick: (usize, bool) = {
 			let pick_box = rand::thread_rng().gen_range(0, 3);
-			(
-				pick_box,
-				match pick_box {
-					0 => boxes.box_1,
-					1 => boxes.box_2,
-					2 => boxes.box_3,
-					_ => panic!("Error picking box on switch"),
-				},
-			)
+			(pick_box, boxes[pick_box])
 		};
 		if first_pick.1 {
 			//If the inital box pick is the winning box, automatically lose since they're going to switch
 			val_lose += 1;
 			continue;
 		} else {
-			let holder = [boxes.box_1, boxes.box_2, boxes.box_3]; //Holds respective true/false values for each box
 			let mut options: Vec<usize> = vec![0, 1, 2];
 			options.remove_item(&first_pick.0);
-			options.drain_filter(|x| !holder[*x]);
+			options.drain_filter(|x| !boxes[*x]);
 			//options = options.into_iter().filter(|x| holder[*x]).collect();
-			if holder[options[0]] {
+			if boxes[options[0]] {
 				val += 1;
 			}
 		}
@@ -114,11 +72,11 @@ fn main() {
 	hang();
 }
 
-fn get_input() -> u32 {
+fn get_input() -> u64 {
 	println!("Please input number of attempts");
 	let mut input = String::new();
 	io::stdin().read_line(&mut input).expect("Line read error");
-	input.trim().parse::<u32>().unwrap()
+	input.trim().parse::<u64>().unwrap()
 }
 
 fn hang() {
